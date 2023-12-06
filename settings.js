a98541bf5c7f04204ea3953750dcb2d5a1d9d342
Qalesya Akyra Rukma Quinnsha <94896069+Akyra2005@users.noreply.github.com>
2023-12-06 19:19:37 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -17,11 +17,11 @@ global.APIKeys = {
 //language 
 global.language = indonesia //change indonesia to english if you don't understand the language used by the bot
 
-global.BOT_TOKEN = "6281571181:AAEZEa5F0LaIxjqcqjFdjQG8KFb0y6HYM1U" //create bot here https://t.me/BotFather and get the bot token
+global.BOT_TOKEN = "6876398827:AAGSoONyRHDMJdeI1QuPNAa3fv_SHZ4fZig" //create bot here https://t.me/BotFather and get the bot token
 global.BOT_NAME = "𝗞𝗶𝗿𝗮𝗿𝗮 𝗕𝗼𝘁" //your bot name
 global.OWNER_NAME = "𝗞𝗲𝗶𝘇𝗵𝗮" //your name
 global.OWNER_NUMBER = "6285163083750" //your telegram number
-global.OWNER = ["https://t.me/Kei_Keizha"] // pastikan username sudah sesuai agar fitur khusus owner bisa di pakai
+global.OWNER = ["https://t.me/Aatar_17"] // pastikan username sudah sesuai agar fitur khusus owner bisa di pakai
 global.THUMBNAIL = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.DONASI = "./image/donasi.jpg" // foto donasi di folder image
 global.lang = language //don't change
